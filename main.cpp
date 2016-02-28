f357adcdb52a9d22fa3f48e83b22cbc23790c79a
Kamil Jastrzab <jastrzabkamil@gmail.com>
2016-02-28 00:13:46 +0000

    removed unused ui file

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QGuiApplication::setApplicationDisplayName(ImageViewer::tr("Image encrypter"));
+    QGuiApplication::setApplicationDisplayName(ImageViewer::tr("Image Encrypter"));
     ImageViewer imageViewer;
     imageViewer.show();
     return app.exec();
